4e111ade39862220336ab9b2ae27bf71199b4623
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-07-08 23:56:32 +0700

    FEAT: create cta heroes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
 import BouncingText from "@/components/BounchingText";
+import CallToAction from "@/components/Heroes/CallToAction";
 import ScrolledImage from "@/components/ScrolledImage";
 
 export default function HomePage() {
   return (
     <div className="grow max-w-7xl px-6 lg:px-8 mx-auto w-full">
+      <CallToAction/>
       <div className="bg-red-400 h-[50vh] w-full mb-16"></div>
       <ScrolledImage/>
       <BouncingText text="huhah"/>
---
+++ components/Heroes/CallToAction.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignIn } from '@fortawesome/free-solid-svg-icons'
+import NavigationLink from '../Buttons/NavigationLink'
+
+const CallToAction = () => {
+  return (
+    <div className="flex flex-col justify-center items-center py-16 gap-7">
+      <Link href={'/'}>
+        <Image
+          src="/logo-no-bg.svg"
+          alt="Logo image"
+          width={79}
+          height={79}
+          priority
+        />
+      </Link>
+      <h1 className="text-[4rem] leading-[4.5rem] text-white text-center max-w-xl m-0">
+        Kelola Uangmu Tanpa Ribet
+      </h1>
+      <NavigationLink
+        href={'/login'}
+        className="flex items-center gap-2 no-underline border border-white/25 hover:bg-dark-secondary py-2 px-4 text-base hover:shadow-2xl  transition-default rounded-lg"
+        activeClass="opacity-100"
+        aria-label="Signin"
+      >
+        <FontAwesomeIcon
+          icon={faSignIn}
+          className="text-orange mr-2"
+          size="lg"
+        />
+        <span>Sign in</span>
+      </NavigationLink>
+      <p className='max-w-lg text-center m-0'>
+        Aplikasi Money Manager modern untuk melacak pemasukan, pengeluaran, dan
+        mencapai tujuan finansial.
+      </p>
+    </div>
+  )
+}
+
+export default CallToAction
