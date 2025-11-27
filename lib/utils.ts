a4bf8d439c0352a39d1c763b6ff3a377e9859c21
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-11-27 23:49:43 +0700

    FEAT: format big number

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import clsx, { ClassValue } from 'clsx'
+import { precisionPrefix, format as d3Format } from 'd3'
 import { format } from 'date-fns'
 import { enUS, id } from 'date-fns/locale'
 import { twMerge } from 'tailwind-merge'
@@ -31,4 +32,21 @@ export function getMonthNameFromIndex(
   const date = new Date( 2000, monthIndex, 1 )
   
   return format( date, formatStr, { locale } )
+}
+
+const precis = precisionPrefix( 1e3, 1.3e6 )
+
+export const formatBigNumber = ( raw: number | string, whole = false ) => {
+  const n = Number( raw );
+
+  const hasDecimals = String( raw ).includes( "." ) && !whole;
+  const smallFormat = hasDecimals
+    ? d3Format( ".2f" )     // round to 2 decimal places
+    : d3Format( ".0f" );    // no decimals
+
+  if ( Math.abs( n ) >= 1e1 ) {
+    return d3Format( `.${precis}~s` )( n );
+  }
+
+  return smallFormat( n );
 }
