66360374733ab0747b7e87e114fa79c0d466f499
Ian Febi Sastrataruna <ianfebi01@gmail.com>
2025-11-27 23:44:08 +0700

    FEAT: get month name from index

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
 import clsx, { ClassValue } from 'clsx'
+import { format } from 'date-fns'
+import { enUS, id } from 'date-fns/locale'
 import { twMerge } from 'tailwind-merge'
 
 /** Merge classes with tailwind-merge with clsx full feature */
@@ -12,4 +14,21 @@ export function cn( ...inputs: ClassValue[] ) {
 
 export function openNewTab( url: string ) {
   window.open( url )
+}
+
+/**
+ * Get month name from month index
+ */
+
+type SupportedLocale = typeof enUS | typeof id
+
+export function getMonthNameFromIndex(
+  monthIndex: number,
+  locale: SupportedLocale = enUS,
+  formatStr: string = 'LLL'
+) {
+
+  const date = new Date( 2000, monthIndex, 1 )
+  
+  return format( date, formatStr, { locale } )
 }
